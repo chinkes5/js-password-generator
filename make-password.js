d6f6596f6c669fa4512e84b239dfcb3ec862440c
John Chinkes <24719595+chinkes5@users.noreply.github.com>
2023-01-01 17:12:27 -0700

    some prettyifying and additional function options

--- make-password.js
+++ make-password.js
@@ -1,19 +1,58 @@
 const symbols = ["!", "@", "#", "$", "%", "^", "&", "(", ")"];
-let number = Math.floor(Math.random() * 99);
 
-function makePassword(longer = false) {
-    const passArray = [symbols[Math.floor(Math.random() * symbols.length)], number];
-    if (longer == true) {
-        passArray.push(sixLetter[Math.floor(Math.random() * sixLetter.length)].value);
-        passArray.push(sixLetter[Math.floor(Math.random() * sixLetter.length)].value);
+/*function makePassword(longerWords = false, wordCount = 2, noSymbol = false, noNumber = false) {
+    //some checks on how many words requested
+    if (isNaN(wordCount)) {
+        throw "No number passed for word count!"
     }
-    else {
-        passArray.push(fiveLetter[Math.floor(Math.random() * fiveLetter.length)].value);
-        passArray.push(fiveLetter[Math.floor(Math.random() * fiveLetter.length)].value);
+    //keep the word count in this range
+    if (wordCount < 2) {
+        wordCount = 2
+    }
+    if (wordCount > 10) {
+        wordCount = 10
+    }
+
+    //had to make the array outside of conditions for some reason
+    const passArray = [Math.floor(Math.random() * 99)];
+    if (noNumber == true) {
+        //testing for true so I can remove the number
+        passArray.pop();
     }
     
-    return passArray.sort(function () { return 0.5 - Math.random() }).join('');
-    // return password;
-}
+    if (noSymbol == false) {
+        //testing for false so I can add to the array with a symbol
+        passArray.push(symbols[Math.floor(Math.random() * symbols.length)]);
+    }
+    
+    if (longerWords == true) {
+        //testing for true to give the longer words
+        for (let index = 0; index < wordCount; index++) {
+            //loop thru the word count
+            passArray.push(sixLetter[Math.floor(Math.random() * sixLetter.length)].value);
+        }
+    }
+    else {
+        //the default is the shorter words
+        for (let index = 0; index < wordCount; index++) {
+            //loop thru the word count
+            passArray.push(fiveLetter[Math.floor(Math.random() * fiveLetter.length)].value);
+        }
+    }
 
-document.getElementById("password").innerHTML = makePassword()
+    //mix up the array and return as one string
+    return passArray.sort(function () { return 0.5 - Math.random() }).join('');
+}*/
+
+// document.getElementById("password").innerHTML = makePassword()
+const password = async () => {
+    let response = await fetch("https://passwords.chinkes.workers.dev/password", {
+        headers: {
+            'Content-Type': 'application/json',
+            'Origin': window.location.origin+window.location.host
+          }
+    })
+    .then(response => response.text())
+    // let myJson = await response.json();
+}
+document.getElementById("password").innerHTML = password().then
